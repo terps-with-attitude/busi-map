f1cc3e2a2ba9b4e15ccc044249ab836126821cca
Affan Hussain <affan98@gmail.com>
2016-11-05 10:50:42 -0400

    Major bugfixes

--- pullInData.js
+++ pullInData.js
@@ -4,27 +4,40 @@ var num = 0;
 
 function pullInData() {
 
-	dict = [];
-	dc = new google.maps.LatLng(-33.8665433,151.1956316);
+	dict = new Map();
+	dc = new google.maps.LatLng(38.907864,-77.072151);
 	mapa = new google.maps.Map(document.getElementById('mapa'), {
 	center: dc,
 	zoom: 15
-});
-	var request = {
-	location: dc,
-	query: categories[num++]
-	};
-	var service = new google.maps.places.PlacesServices(mapa);
+ 	});
+
+
+	var request = { location: dc,
+					query: categories[num++]
+				  };
+
+
+	var service = new google.maps.places.PlacesService(mapa);
+
+
 	for (var i = 0; i < categories.length;i++) {
 		service.textSearch(request, callback);
 	}
+
+
+	var mapIter = dict.keys();
+
+
+
+
+	return dict;
 }
+
+
+
 
 function callback(results, status) {
 	if (status == google.maps.places.PlacesServiceStatus.OK) {
-	dict.push({
-		category: categories[num],
-		arrayOfPlaces: results
-	});
-}
+		dict.set(categories[num],results);
+   }
 }
