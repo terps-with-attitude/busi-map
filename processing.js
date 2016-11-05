4e48aee68fa4cc77475c3619af3a0256300fa0ba
Anton Pozharskiy <apozharski@gmail.com>
2016-11-05 19:30:27 -0400

    Recovered functionality.

--- processing.js
+++ processing.js
@@ -1,13 +1,59 @@
 
+// // returns map to array of markers.
+// function processPlaces(placeMap)
+// {
+
+// 	var outputMap = new Map();
+// 	for (var category in placeMap.keys()) 
+// 	{
+// 		var outputArray = new Array();
+// 	 	for (var place in placeMap.get()) 
+// 	 	{
+// 	 		var placeMarker = new google.maps.Marker({
+// 	 			map : null,	
+// 	 			position : place.geometry.location 
+// 	 		});
+// 	 		outputArray.push(placeMarker);
+// 	 	}
+// 	 	outputMap.set(category, outputArray);
+// 	}
+
+// 	return outputMap;
+	
+// }
+
+// // creates map to be displayed.
+// function createMap(categoryArray, markerMap)
+// {
+// 	var map = new google.maps.Map(document.getElementById('map-container'), {
+// 		zoom: 12,
+// 		center: {lat: 38.907864, lng:  -77.072151}
+// 	});
+
+// 	for(var category in categoryArray)
+// 	{
+// 		for(marker in markerMap.get(category))
+// 		{
+// 			marker.setMap(map);
+// 		}
+// 	}
+// }
+
+
+
 // returns map to array of markers.
-function processPlaces(placeMap)
+function processPlaces(placeDict)
 {
 
-	var outputMap = new Map();
-	for (var categoryList in placeMap.values()) 
+
+	var outputDict = new Map();
+
+	setTimeout(console.log(placeDict), 1000);
+	for (var category in placeDict.keys()) 
 	{
+
 		var outputArray = new Array();
-	 	for (var place in categoryList) 
+	 	for (var place in placeDict.get(placeDict.keys()[category])) 
 	 	{
 	 		var placeMarker = new google.maps.Marker({
 	 			map : null,	
@@ -15,26 +61,26 @@ function processPlaces(placeMap)
 	 		});
 	 		outputArray.push(placeMarker);
 	 	}
-	 	outputMap.set(category, outputArray);
+	 	outputDict.set(placeDict.keys()[category], outputArray);
 	}
 
-	return outputMap;
+	return outputDict;
 	
 }
 
-// creates map to be displayed.
-function createMap(categoryArray, markerMap)
+// updates map to be displayed.
+function updateMap(categoryArray, markerMap, googleMap)
 {
-	var map = new google.maps.Map(document.getElementById('map-container'), {
-		zoom: 12,
-		center: {lat: 38.907864, lng:  -77.072151}
-	});
+	var map =  googleMap;
+
 
 	for(var category in categoryArray)
 	{
+
 		for(marker in markerMap.get(category))
-		{
+		{			
 			marker.setMap(map);
+			//console.log(marker.map);
 		}
 	}
 }
--- pullInData.js
+++ pullInData.js
@@ -1,54 +1,107 @@
-var categories = new Array("fast food", "fine dining", "buffet");
-var dc, mapa, dict;
-var num = 0;
+// var categories = new Array("fast food", "fine dining", "buffet");
+// var dc, mapa, dict;
+// var num = 0;
 
-function pullInData() {
+// function pullInData() {
 
-	dict = new Map();
-	dc = new google.maps.LatLng(38.907864,-77.072151);
-	mapa = new google.maps.Map(document.getElementById('mapa'), {
-	center: dc,
-	zoom: 15
- 	});
-
-
-	// var request = { location: dc,
-	// 				query: categories[num++]
-	// 			  };
+// 	dict = new Map();
+// 	dc = new google.maps.LatLng(38.907864,-77.072151);
+// 	mapa = new google.maps.Map(document.getElementById('mapa'), {
+// 	center: dc,
+// 	zoom: 15
+//  	});
 
 
-	var service = new google.maps.places.PlacesService(mapa);
+// 	// var request = { location: dc,
+// 	// 				query: categories[num++]
+// 	// 			  };
 
 
-	// for (var i = 0; i < categories.length;i++) {
-	// 	service.textSearch(request, callback);
-	// }
+// 	var service = new google.maps.places.PlacesService(mapa);
 
 
-	for(var category in categories)
-	{
-		var request = {
-			location : dc,
-			query : category
-		}
-
-		service.textSearch(request, callback);
-	}
+// 	// for (var i = 0; i < categories.length;i++) {
+// 	// 	service.textSearch(request, callback);
+// 	// }
 
 
-	var mapIter = dict.keys();
+// 	for(var category in categories)
+// 	{
+// 		var request = {
+// 			location : dc,
+// 			query : category
+// 		}
+
+// 		service.textSearch(request, callback);
+// 	}
 
 
-
-
-	return dict;
-}
+// 	var mapIter = dict.keys();
 
 
 
 
-function callback(results, status) {
-	if (status == google.maps.places.PlacesServiceStatus.OK) {
-		dict.set(category,results);
-   }
-}
+// 	return dict;
+// }
+
+
+
+
+// function callback(results, status) {
+// 	if (status == google.maps.places.PlacesServiceStatus.OK) {
+// 		dict.set(category,results);
+//    }
+// }
+
+
+		var googleMap;
+		var infowindow;
+		
+
+      	function pullInData(location, categories, callback) {
+        var dcLocation = location;
+        var dict = new Map();
+
+        googleMap = new google.maps.Map(document.getElementById('map-container'),{
+        	center : dcLocation,
+        	zoom : 12
+        });
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(googleMap);
+
+        for (category in categories) {
+        	//console.log(categories[category]);
+        	var request = {
+				location: dcLocation,
+				type: "restaurant",
+				radius: 2000,
+				query: categories[category]
+        	}
+      		service.textSearch(request, callbackCreator(categories[category], dict));
+      		// for(place in dict.get(categories[category]))
+      		// {
+      		// 	console.log(dict.get(categories[category])[place]);
+      		// }
+        }
+
+        
+        return dict;
+      }
+
+      function callbackCreator(category, dictionary)
+      {
+      	return function(results, status) {
+      		if (status === google.maps.places.PlacesServiceStatus.OK) {
+          		easyA(dictionary,category,results);
+          		console.log(category + "   " + results);
+
+          		// for(place in results)
+          		// {
+          		// 	console.log(results[place]);
+          		// }
+      		}
+      	}
+      }
+
+      
