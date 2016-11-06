e4c1e591928ba74b729d1826f0d3a34fc0f92b5b
AbhishekVel <avelay@terpmail.umd.edu>
2016-11-06 06:14:00 -0500

    Added color markers for each category of search. This includes a legend which shows which color the marker of the category is. Also stopped duplication of category entries.

--- pullInData.js
+++ pullInData.js
@@ -1,119 +1,62 @@
-// var categories = new Array("fast food", "fine dining", "buffet");
-// var dc, mapa, dict;
-// var num = 0;
-
-// function pullInData() {
-
-// 	dict = new Map();
-// 	dc = new google.maps.LatLng(38.907864,-77.072151);
-// 	mapa = new google.maps.Map(document.getElementById('mapa'), {
-// 	center: dc,
-// 	zoom: 15
-//  	});
-
-
-// 	// var request = { location: dc,
-// 	// 				query: categories[num++]
-// 	// 			  };
-
-
-// 	var service = new google.maps.places.PlacesService(mapa);
-
-
-// 	// for (var i = 0; i < categories.length;i++) {
-// 	// 	service.textSearch(request, callback);
-// 	// }
-
-
-// 	for(var category in categories)
-// 	{
-// 		var request = {
-// 			location : dc,
-// 			query : category
-// 		}
-
-// 		service.textSearch(request, callback);
-// 	}
-
-
-// 	var mapIter = dict.keys();
-
-
-
-
-// 	return dict;
-// }
-
-
-
-
-// function callback(results, status) {
-// 	if (status == google.maps.places.PlacesServiceStatus.OK) {
-// 		dict.set(category,results);
-//    }
-// }
-
-
-		var googleMap;
-		var infowindow;
-		
-
-      	function pullInData(location, categories, markerArray, placeDict, googleMap) {
-        var dcLocation = location;
-        
-        
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(googleMap);
-
-
-        clearAll(markerArray);
-        for (category in categories) {
-        	//console.log(categories[category]);
-
-        	var request = {
-				location: dcLocation,
-				type: "restaurant",
-				radius: 2000,
-				query: categories[category]
-        	}
-
-      		service.textSearch(request, callbackCreator(categories[category], markerArray, placeDict, googleMap));
-      		// for(place in dict.get(categories[category]))
-      		// {
-      		// 	console.log(dict.get(categories[category])[place]);
-      		// }
+var infowindow;
+function pullInData(location, categories, markerArray, googleMap, idcategorydict) {
+    var dcLocation = location;
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(googleMap);
+    clearAll(markerArray);
+    for (category in categories) {
+        var request = {
+            location: dcLocation,
+            type: "restaurant",
+            radius: 2000,
+            query: categories[category]
         }
 
-        
-      }
+        service.textSearch(request, callbackCreator(categories[category], idcategorydict));
+    }
 
-      function callbackCreator(category, markerArray, placeDict, googleMap)
-      {
-      	return function(results, status) {
-      		if (status === google.maps.places.PlacesServiceStatus.OK) {
-          		placeDict.set(category, results);
-              for(place in results){
-                              
-                markerArray.push( new google.maps.Marker({
-                 map : map, 
-                 position : results[place].geometry.location 
-                }));
-              }
-      		}
-      	}
-      }
 
-      function clearAll(markerArray) {
+}
 
-        for(marker in markerArray)
-        {
-          markerArray[marker].setMap(null);
+function callbackCreator(category, idcategorydict) {
+    return function (results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (place in results) {
+                createMarker(results[place], category, idcategorydict);
+            }
+
         }
+    }
+}
 
-        markerArray = [];
+function createMarker(place, category, idcategorydict) {
+    var icon = 'markers/' + idcategorydict[category] + '.png';
+    console.log(icon);
 
-      }
+    var marker = new google.maps.Marker({
+        map: map,
+        icon: icon,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        var name = place.name;
+        var rating = place.rating;
+        var html = "<b>" + name + "</b> <br/>" + 'Rating: '+ rating;
+        infowindow.setContent(html);
+        infowindow.open(map, this);
+    });
+    markerArray.push(marker);
+}
+
+function clearAll(markerArray) {
+
+    for (marker in markerArray) {
+        markerArray[marker].setMap(null);
+    }
+
+    markerArray = [];
+
+}
 
 
-      
